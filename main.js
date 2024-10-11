01cf244b42de7bddf5f2f12fb8c5add8a006e339
bazylevnik0 <bazylevnik0@gmail.com>
2024-10-11 22:37:54 +0300

    fixing vr camera position

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 
-console.log(7)
+console.log(8)
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
