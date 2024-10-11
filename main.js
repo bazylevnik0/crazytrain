cd70ba751f0f9956b4222b4462039499382ff6ca
bazylevnik0 <bazylevnik0@gmail.com>
2024-10-11 23:22:29 +0300

    fixing vr camera position

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 
-console.log(11)
+console.log(12)
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -57,10 +57,9 @@ var p1_start = new THREE.Vector3(0,0,-1);
 var p2_start = new THREE.Vector3(0,0,-3);
 add_segment(p1_start, p2_start);
 let direction = new THREE.Vector3(0,0,-2)
-var p1 = p2_start.clone();
-var p2 = p1.clone();
+var p1 = p1_start.clone();
+var p2 = p2_start.clone();
 let p0 = p1.clone();
-camera_group.position.set(p0);
 function animate() {
 	if (renderer.xr.isPresenting) {
 		if(iter%10==0) {
@@ -84,13 +83,16 @@ function animate() {
 			p2 = new_p2.add(p1);
 			add_segment(p1,p2);
 		}
+		
 		let length = p0.distanceTo(camera_group.position);
 		let delta  = length/10;
 		let direction_position = new THREE.Vector3(0,0,0)
 			direction_position.subVectors( p0, camera_group.position ).normalize();
 		let new_position = camera_group.position;	
 			new_position.add( direction_position.multiplyScalar( delta ) );
-			camera_group.position.set(new_position);
+			camera_group.position.x = new_position.x;
+			camera_group.position.y = new_position.y;
+			camera_group.position.z = new_position.z;
 	}
 	renderer.render( scene, camera ); iter++; 
 }
