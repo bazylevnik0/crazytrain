25c5c1354f1388d229ddd13fa4c317f8676131c2
bazylevnik0 <bazylevnik0@gmail.com>
2024-10-12 14:14:54 +0300

    fixing vr camera position

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 
-console.log(15)
+console.log(16)
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -42,16 +42,16 @@ function add_segment (p1,p2,direction) {
 	points.push( new THREE.Vector3( -2+p1.x,  1+p1.y, 0+p1.z ) );
 
 	let geometry = new THREE.BufferGeometry().setFromPoints( points );
+	if (direction) {
+		geometry.rotateX = direction.x;
+		geometry.rotateY = direction.y;
+		geometry.rotatez = direction.z;
+	}
 	var segment = new THREE.Line(geometry, new THREE.LineBasicMaterial({
 		color: 0x0000ff,
 		linewidth: 1,
 	}));
 	segment.computeLineDistances();
-	if (direction) {
-		segment.rotation.x = direction.x;
-		segment.rotation.y = direction.y;
-		segment.rotation.z = direction.z;
-	}
 	scene.add( segment );
 	segments.push(segment);
 }
