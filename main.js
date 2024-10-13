594f27ded494300f2cc9b77448c9b6b2b495d618
bazylevnik0 <bazylevnik0@gmail.com>
2024-10-13 21:01:23 +0300

    creating environment

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-console.log(59)
+console.log(60)
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -38,9 +38,9 @@ function add_segment (position, rotation) {
 		  cube.position.x = position.x;
 		  cube.position.y = position.y;
 		  cube.position.z = position.z;
-		  //cube.rotation.x = rotation.x;
-		  //cube.rotation.y = rotation.y;
-		  //cube.rotation.z = rotation.z+ Math.PI/2;
+		  cube.rotation.x = rotation.x;
+		  cube.rotation.y = rotation.y;
+		  cube.rotation.z = rotation.z+Math.PI/3;
 		  //cube.geometry.lookAt( rotation);
 	scene.add( cube ); 
 }
@@ -65,7 +65,7 @@ function animate() {
 			direction_position = position_path.clone();	
 			direction_position.add ( direction_rotation );
 			position_path = direction_position.clone();		
-			position_new  = position_path.clone();
+			position_new  = position_path.clone();	
 			add_segment( position_path, direction_rotation );
 		}
 		length = position_old.distanceTo( position_new );
