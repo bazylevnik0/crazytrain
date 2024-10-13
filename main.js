82afe7e765419594eb653e2f763680baf1a855a4
bazylevnik0 <bazylevnik0@gmail.com>
2024-10-13 14:09:09 +0300

    adding more segments

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 import * as THREE from 'three';
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 
-console.log(36)
+console.log(37)
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 	  camera.position.z = 0;
 let camera_group = new THREE.Group();
-	camera_group.position.set( 0, 0, 0);
+	camera_group.position.x = 0;
+	camera_group.position.y = 0;
+	camera_group.position.z = 0;
 	camera_group.add( camera );
 	scene.add(camera_group);
 
@@ -24,7 +26,9 @@ function add_segment (position, rotation) {
 		  material.wireframe = true;
 	const edges = new THREE.EdgesGeometry( geometry ); 
 	const line  = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) ); 
-		  line.position.set( position );
+		  line.position.x = position.x;
+		  line.position.y = position.y;
+		  line.position.z = position.z;
 		  line.geometry.lookAt( rotation);
 	scene.add( line );
 }
@@ -34,19 +38,20 @@ let position_new = position_path.clone();
 let i = 0; let length = position_path.distanceTo(position_new); let delta  = length/10;
 	
 function animate() {
-	if (renderer.xr.isPresenting) {
+	//if (renderer.xr.isPresenting) {
 	let position_old = camera_group.position.clone();
 	
 	if (i%10==0) {
 		let direction_rotation = new THREE.Vector3();
 			renderer.xr.getCamera().getWorldDirection(direction_rotation);
-		let direction_position = position_path.clone();	
+		for (let j = 0; j < 10; j++) {
+			let direction_position = position_path.clone();	
 			direction_position.add ( direction_rotation );
 			position_path = direction_position.clone();		
 			position_new  = position_path.clone();
 	
-		add_segment(position_path, direction_rotation);
-		
+			add_segment(position_path, direction_rotation);
+		}
 		length = position_old.distanceTo(position_new);
 		delta  = length/10;
 	} 
@@ -58,6 +63,6 @@ function animate() {
 		camera_group.position.x = position_current.x;		
 		camera_group.position.y = position_current.y;		
 		camera_group.position.z = position_current.z;		
-	}	
+	//}	
 	renderer.render( scene, camera ); i++;
 }
