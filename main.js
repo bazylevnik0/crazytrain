ee58782ba89da420509cb9a97c06be93a59b6381
bazylevnik0 <bazylevnik0@gmail.com>
2024-10-14 17:54:21 +0300

    add plane

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { VRButton } from 'three/addons/webxr/VRButton.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 
-console.log(84)
+console.log(85)
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -26,6 +26,7 @@ document.body.appendChild( renderer.domElement );
 
 let loader = new GLTFLoader();
 let model;
+let plane;
 new RGBELoader()
 	.load( 'back.hdr', function ( texture ) {
 
@@ -37,11 +38,11 @@ new RGBELoader()
 		let texture_grass = new THREE.TextureLoader().load( "grass.png" );
 		texture_grass.wrapS = THREE.RepeatWrapping;
 		texture_grass.wrapT = THREE.RepeatWrapping;
-		texture_grass.repeat.set( 1000, 1000 );
+		texture_grass.repeat.set( 10, 10 );
 		
 		const geometry = new THREE.PlaneGeometry( 10000, 10000 );
 		const material = new THREE.MeshBasicMaterial( {color: 0x00ff00, side: THREE.DoubleSide, map: texture_grass} );
-		const plane = new THREE.Mesh( geometry, material );
+		plane = new THREE.Mesh( geometry, material );
 		plane.position.y = -5;
 		plane.rotation.x = Math.PI/2
 		scene.add( plane );
@@ -76,7 +77,6 @@ let direction_rotation = new THREE.Vector3();
 let direction_position = new THREE.Vector3();
 
 function animate() {
-		if (camera_group.position.y < 1) camera_group.position.y = 1
 		let position_old = camera_group.position.clone();
 		// Each 10s frame add new segments
 		if ( i%10 == 0 ) {
@@ -103,6 +103,9 @@ function animate() {
 			camera_group.position.x = position_current.x;		
 			camera_group.position.y = position_current.y+0.25;		
 			camera_group.position.z = position_current.z+0.5;		
+			
+			if (camera_group.position.y < 2) plane.position.y = camera_group.position.y - 3; 
+		
 	renderer.render( scene, camera ); i++;
 	
 	if (segments.length > 100) scene.remove(segments.shift());
