08871f6171b2bdc2422c85eb4f2601a9564423bd
bazylevnik0 <bazylevnik0@gmail.com>
2024-10-14 15:14:25 +0300

    adding railroad

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-console.log(74)
+console.log(75)
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
